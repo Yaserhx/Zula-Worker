f417bb25e6809c6ca4459f72562d970067f48fc1
alikhaje5731 <143724054+alikhaje5731@users.noreply.github.com>
2024-01-21 23:48:38 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '652da35d-b59d-4ab0-af59-cb90066dcc10';
+let userID = 'e2a195d2-90a4-4713-9a37-46b0909db318';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
